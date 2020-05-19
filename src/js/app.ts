685ee8199d74d0357566fa6ec7896ce12a660877
LarzDosan <rey_gato@hotmail.com>
2020-05-19 12:30:33 -0400

    agregado el archivos para mejorar el flujo de trabajo

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,2 +1,4 @@
 import "../scss/styles.scss";
 //?Here go you ts code
+//* import '../static/image.jpg';
+//* import 'image.jpg';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const htmlPlugin = require('html-webpack-plugin');
 const mini_css_extract_plugin = require('mini-css-extract-plugin')
 module.exports = {
-    entry: './src/js/app.ts',
+    entry: './src/ts/app.ts',
     devtool: 'inline-source-map',
     devServer: {
         port: 5500
@@ -21,7 +21,43 @@ module.exports = {
                     { loader: "css-loader" },
                     { loader: "sass-loader" },
                 ]
-            }
+            }, {
+                test: /\.(jpg|png|gif|jpge)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'static/',
+                            useRelativePath: true
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
+            },
         ]
     },
     plugins: [
